a7c2cb101137f172ce5d427c38bcd6e49dc5560c
dmitry <dp@webactives.ru>
2018-10-26 00:29:14 +0300

    ...

--- src/js/components/MotionParticle/code/particle.js
+++ src/js/components/MotionParticle/code/particle.js
@@ -4,6 +4,9 @@ class Particle{
     constructor(data, scene){
         this.scene = scene;
         this.data = data;
+        this.count = 0;
+        this.time = []
+        this.res = 0
     }
     createParticles(i) {
         const geometry = new THREE.SphereBufferGeometry(this.data.props[i].size, 16, 16);
@@ -13,15 +16,26 @@ class Particle{
         this.motionParticle(i);
     }
     motionParticle(i) {
+        this.count++;
         let data = {
             i: i,
             radius: Math.abs(physics.inRad(this.data.props[i].radius)),
             angle: this.data.props[i].angle,
             charge: this.data.props[i].charge
         };
+        let start = window.performance.now()
         this.calculationMotion({...data, axis: 'x', horizontal: true});
         this.calculationMotion({...data, axis: 'y', horizontal: false});
         this.calculationMotion({...data, axis: 'z', horizontal: true});
+        let end = window.performance.now();
+        if(this.count < 2000) this.time.push(end - start)
+        if(this.count === 2010) console.log( this.getTime() )
+    }
+    getTime(){
+        this.time.forEach((el)=>{
+            this.res += el
+        })
+        return this.res/this.time.length
     }
     calculationMotion({radius, angle, charge, horizontal, axis, i}){
         const square = this.getSquare(i, axis, horizontal);
