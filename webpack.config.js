fbe5dcbc28a97fc55f3d2065c95050e42b94ce7d
dmitry <dp@webactives.ru>
2018-11-01 22:53:10 +0300

    update webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,13 @@
-const isProd = process.env.NODE_ENV === 'production';
+const devMode = process.env.NODE_ENV !== 'production';
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
     entry: './src/js/main.js',
     output: {
-        path: __dirname + '/public',
-        publicPath: '/',
-        filename: 'bundle.js'
+        path: path.resolve(__dirname, 'public'),
+        filename: '[name].[chunkhash].js'
     },
     module: {
         rules: [
@@ -25,16 +28,25 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [
-                    { loader: "style-loader" },
-                    { loader: "css-loader" },
-                    { loader: "sass-loader"}
-                ]
+                use:  [  'style-loader', MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader']
             }
         ]
     },
-    resolve: {
-        extensions: ['*', '.js', '.jsx']
-    },
+    plugins: [
+        new CleanWebpackPlugin('public', {} ),
+        new MiniCssExtractPlugin({
+            filename: 'style.[contenthash].css',
+        }),
+        new HtmlWebpackPlugin({
+            inject: false,
+            hash: true,
+            template: './src/index.html',
+            filename: 'index.html'
+        })
+    ],
     devtool: 'source-map',
+    performance: { hints: false },
+    resolve: {
+        extensions: ['*', '.js', '.jsx', 'scss']
+    },
 };
