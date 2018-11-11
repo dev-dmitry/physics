f040499b963c291807d7d31984ddfff8c60333ac
dmitry <dp@webactives.ru>
2018-11-11 11:49:21 +0300

    ...

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,8 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 import '@babel/polyfill';
 import App from './App';
 import '../sass/app.scss';
 import './core/physicsBase';
 
-render(<App/>, document.getElementById('app'));
+render(<App />, document.getElementById('app'));
