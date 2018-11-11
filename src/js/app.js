0b9eb7dca313e895356f3a146dc6c789f4e581b4
dmitry <dp@webactives.ru>
2018-11-11 12:43:28 +0300

    ...

--- src/js/app.js
+++ src/js/app.js
@@ -4,6 +4,5 @@ import '@babel/polyfill';
 import App from './App';
 import '../sass/app.scss';
 import './core/physicsBase';
-console.log('test');
 
 render(<App />, document.getElementById('app'));
